99d299bb547cb658da6c787a2a9449991594063e
Sergei <sergmotol@gmail.com>
2020-07-22 14:58:11 +0300

    readme

--- js/my-tabs.js
+++ js/my-tabs.js
@@ -2,15 +2,6 @@ const tabs = function(node){
     const tabsWrapper = document.querySelector(node),
         tabHeader = tabsWrapper.querySelector('.tabheader'),
         tabContent = tabsWrapper.querySelector('.tabcontent'),
-        _showTab = function(target){
-            const tabId = target.getAttribute('data-tabtarget');
-
-            tabContent
-                .querySelector(`[data-tabid=${tabId}]`)
-                .classList.add('tabcontent__item-show');
-    
-            target.classList.add('tabheader__item-show');
-        },
         _hideTabs = function(){
             const tabHeaderItems = tabHeader.querySelectorAll('.tabheader__item'),
             tabContentItems = tabContent.querySelectorAll('.tabcontent__item');
@@ -22,13 +13,29 @@ const tabs = function(node){
             tabContentItems.forEach((item, i) => {
                 item.classList.remove('tabcontent__item-show');
             });
+        },
+        _showTab = function(target){
+            const tabId = target.getAttribute('data-tabtarget');
+            const activeTab = tabHeader.querySelector('.tabheader__item-show');
+
+            if(target == activeTab){
+                return;
+            }
+
+            _hideTabs();
+
+            tabContent
+                .querySelector(`[data-tabid=${tabId}]`)
+                .classList.add('tabcontent__item-show');
+    
+            target.classList.add('tabheader__item-show');
         };
+
     
     tabHeader.addEventListener('click', (e) => {
         const target = e.target;
 
         if(target && target.classList.contains('tabheader__item')){
-            _hideTabs();
             _showTab(target);
         }
     });
